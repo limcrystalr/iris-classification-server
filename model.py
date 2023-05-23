ed9c9b417dd298bd2cefd88f3fb707ea9f541015
limcrystalr <103323000+limcrystalr@users.noreply.github.com>
2023-05-23 11:45:09 +0900

    4th commit

--- model.py
+++ model.py
@@ -1,7 +1,9 @@
-import datetime
-from typing import Optional, Iterable, Union, Counter
-import collections 
 import math
+import datetime
+import collections
+from typing import Optional, Iterable, Union, Counter, cast
+import weakref
+from model import Sample 
 
 
 class Sample:
@@ -22,15 +24,12 @@ class Sample:
         self.classification: Optional[str] = None
 
     def __repr__(self) -> str:
-       
         return (
             f"{self.__class__.__name__}("
             f"sepal_length={self.sepal_length}, "
             f"sepal_width={self.sepal_width}, "
             f"petal_length={self.petal_length}, "
             f"petal_width={self.petal_width}, "
-            f"species={self.species!r}"
-            
             f")"
         )
     
@@ -40,25 +39,23 @@ class Sample:
     def matches(self) -> bool:
         return self.species == self.classification
 
-class TrainingknownSample:
-    pass
 
 class KnownSample(Sample):
     def __init__(
         self,
+        species: str,
         sepal_length: float,
         sepal_width: float,
         petal_length: float,
         petal_width: float,
-        species: str,
     ) -> None:
-         super().__init__(
-             sepal_length=sepal_length,
-             sepal_width=sepal_width,
-             petal_length=petal_length,
-             petal_width=petal_width
-         )
-         self.species = species
+        super().__init__(
+            sepal_length=sepal_length,
+            sepal_width=sepal_width,
+            petal_length=petal_length,
+            petal_width=petal_width
+        )
+        self.species = species
 
     def __repr__(self) -> str:
         return (
@@ -68,21 +65,48 @@ class KnownSample(Sample):
             f"petal_length={self.petal_length}, "
             f"petal_width={self.petal_width}, "
             f"species={self.species!r}"
-      
             f")"
         )
 
+
 class UnknownSample(Sample):
-    """Sample provided by an user, not yet classified"""
+    """Sample provided by an user, not yet classified."""
+    @classmethod
+    def from_dict(cls,row: dict[str,str]) -> "UnknownSample":
+        if set(row.keys()) != {
+            "sepal_length", "sepal_width", "petal_length","petal_width"
+        }:
+            raise InvaildSampleError(f"invalid fields in {row!r}")
+        try:
+            return cls(
+                sepal_length=float(row["sepal_length"])
+                sepal_width=float(row["sepal_width"])
+                petal_length=float(row["petal_pass
+
+
+class TrainingKnownSample:
     pass
 
-class ClassifiedSample(Sample):
-    def __init__(self, classification, sample: UnknownSample) ->None:
+
+class TestingKnownSample(KnownSample):
+    @classmethod
+    def from.dict_(cls,row:dict[str,str]) -> "TrainingKnownSample" :
+        return 
+    def __init__(
+        self,
+        species: str,
+        sepal_length: float,
+        sepal_width: float,
+        petal_length: float,
+        petal_width: float,
+        classification: Optional[str] = None
+    ) -> None:
         super().__init__(
-            self.sepal_length = sample.sepal_length
-            self.sepal_width = sample.sepal_width
-            self.petal_length = sample.petal_length
-            self.petal_width = sample.petal_width
+            species=species,
+            sepal_length=sepal_length,
+            sepal_width=sepal_width,
+            petal_length=petal_length,
+            petal_width=petal_width
         )
         self.classification = classification
 
@@ -93,30 +117,28 @@ class ClassifiedSample(Sample):
             f"sepal_width={self.sepal_width}, "
             f"petal_length={self.petal_length}, "
             f"petal_width={self.petal_width}, "
-            f"species={self.species!r}"
+            f"species={self.species!r}, "
             f"classification={self.classification!r}"
+            f")"
         )
 
-class TestKnownSample(KnownSample):
-    def __init__( self,
-        sepal_length: float,
-        sepal_width: float,
-        petal_length: float,
-        petal_width: float,
-        classification: Optional[str] = None
-    ) -> None
+    def matches(self) -> bool:
+        return self.species == self.classification
+    
+    @classmethod
+    def from_dict(cls,row: dict[str,str]) -> "TestingKnownSample":
+        return cast(TestingKnownSample, super().from_dict)
 
-    super().__init__(self.sepal_length = sepal_length
-        self.sepal_width = sepal_width
-        self.petal_length = petal_length
-        self.petal_width = petal_width
-
-    )
-    self.classification= classification
-    pass
-
-    def __init__(self) -> str:
-        pass
+class ClassifiedSample(Sample):
+    """for user"""
+    def __init__(self, classification: str, sample: UnknownSample) -> None:
+        super().__init__(
+            sepal_length=sample.sepal_length,
+            sepal_width=sample.sepal_width,
+            petal_length=sample.petal_length,
+            petal_width=sample.petal_width
+        )
+        self.classification = classification
 
     def __repr__(self) -> str:
         return (
@@ -125,70 +147,69 @@ class TestKnownSample(KnownSample):
             f"sepal_width={self.sepal_width}, "
             f"petal_length={self.petal_length}, "
             f"petal_width={self.petal_width}, "
-            f"species={self.species!r}"
             f"classification={self.classification!r}"
+            f")"
         )
-    def matches(self) ->bool:
-        return self.species == self.classification
 
 
 class Distance:
-    def distance(self, s1:Sample,s2: Sample) -> float:
+    def distance(self, s1: Sample, s2: Sample) -> float:
         pass
 
 class ED(Distance):
-    def distance(self, s1:Sample,s2: Sample) -> float:
+    def distance(self, s1: Sample, s2: Sample) -> float:
         return math.hypot(
             s1.sepal_length - s2.sepal_length,
             s1.sepal_width - s2.sepal_width,
             s1.petal_length - s2.petal_length,
             s1.petal_width - s2.petal_width
         )
-    pass
-
 
 class MD(Distance):
-    def distance(self, s1:Sample,s2: Sample) -> float:
+    def distance(self, s1: Sample, s2: Sample) -> float:
         return sum([
             abs(s1.sepal_length - s2.sepal_length),
             abs(s1.sepal_width - s2.sepal_width),
             abs(s1.petal_length - s2.petal_length),
             abs(s1.petal_width - s2.petal_width)
         ])
+    
+class CD:
+    """TODO"""
     pass
 
-class CD:
-    pass
 class SD:
     pass
 
 class Hyperparameter:
-    def __init__(self, k: int,algorithm: Distance, training: "TrainingData") -> None:
+    def __init__(self, k: int, algorithm: Distance, training: "TrainingData") -> None:
         self.k = k
         self.algorithm = algorithm
         self.data: TrainingData = training
         self.quality: float
 
-    def classify(self, sample: Union[UnknownSample, TestingKnownSample]): -> str:
-        training_data = self,data
+    def classify(self, sample: Union[UnknownSample, TestingKnownSample]) -> str:
+        """k-NN algorithm"""
+        training_data = self.data()
         if not training_data:
-            raise RuntimeError("No Training object")
+            raise RuntimeError("No TrainingData object!")
         distances: list[tuple[float, TrainingKnownSample]] = \
             sorted(
-                    (self,algorithm.distance(sample, known),known) for known in training_data.training 
+                (self.algorithm.distance(sample, known), known) 
+                for known in training_data.training
             )
-        """TODO: k-NN 알고리즘"""
         k_nearest: tuple[str] = (known.species for _, known in distances[:self.k])
         frequency: Counter[str] = collections.Counter(k_nearest)
-        best_fit, *others = frequency.most_common()
+        best_fit, *others = frequency.most_common()  # ("a", 5)
         species, votes = best_fit
         return species
+
     def test(self) -> None:
         training_data: Optional["TrainingData"] = self.data
         if not training_data:
             raise RuntimeError("")
         pass_count, fail_count = 0, 0
-        for sample in self.data.testing_data.testing:
+        for sample in self.training_data.testing:
             sample.classification = self.classify(sample)
             if sample.matches():
                 pass_count += 1
@@ -205,29 +226,38 @@ class TrainingData:
         self.training: list[Sample] = []
         self.testing: list[Sample] = []
         self.tuning: list[Hyperparameter] = []
-
+    @classmethod
+    def from_dict(cls, row) -> "KnownSample":
+        if row["species"] not in {"Iris-setosa", "Iris-versicolour", "Iris-virginica"}:
+            raise InvalidSampleError(f"invalid species in{row!r}")
+        try:
+            return cls(
+                species=row["species"]
+                sepal_length=float(row["sepal_length"])
+                sepal_width=float(row["sepal_width"])
+                petal_length=float(row["petal_length"])
+                petal_width=float(row["petal_width"])
+            )
     def load(self, raw_data_source: Iterable[dict[str, str]]) -> None:
         for n, row in enumerate(raw_data_source):
-            if n%5 ==0:
-                test +TestingKnownSample()
-                species = row["species"]
-                sepal_length=float(row["sepal_length"]),
-                sepal_width=float(row["sepal_width"]),
-                petal_length=float(row["petal_length"]),
-                petal_width=float(row["petal_width"]),
-                species=row["species"]
-            )
-            
+            if n % 5 == 0:
+                test = TestingKnownSample(
+                    species=row["species"]
+                    sepal_length=float(row["sepal_length"]),
+                    sepal_width=float(row["sepal_width"]),
+                    petal_length=float(row["petal_length"]),
+                    petal_width=float(row["petal_width"]),
+                )
                 self.testing.append(test)
             else:
-                test  (species = row["species"]
-                sepal_length=float(row["sepal_length"]),
-                sepal_width=float(row["sepal_width"]),
-                petal_length=float(row["petal_length"]),
-                petal_width=float(row["petal_width"]),
-                
-                )
-                self.training.append(test)
+                train = TrainingKnownSample(
+                    species=row["species"]
+                    sepal_length=float(row["sepal_length"]),
+                    sepal_width=float(row["sepal_width"]),
+                    petal_length=float(row["petal_length"]),
+                    petal_width=float(row["petal_width"]),
+                )                
+                self.training.append(train)
         self.uploaded = datetime.datetime.now(tz=datetime.timezone.utc)
 
     def test(self, parameter: Hyperparameter) -> None:
@@ -235,12 +265,10 @@ class TrainingData:
         self.tuning.append(parameter)
         self.tested = datetime.datetime.now(tz=datetime.timezone.utc)
 
-    def classify(self, parameter: Hyperparameter, sample: Sample) -> Sample:
-        classification = parameter.classify(sample)
-        sample.classify(classification)
-        return sample
-
-
+    def classify(self, parameter: Hyperparameter, sample: UnknownSample) -> ClassifiedSample:
+        return ClassifiedSample(
+            classification=parameter.classify(sample), sample=sample            
+        )
 
 
 test_sample = """
